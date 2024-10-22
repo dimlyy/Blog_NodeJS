185cae36a29fd17bd80ca0dfc334a28226086441
dimlyy <dimlytad@gmail.com>
2024-10-22 16:19:00 +0700

    Your commit message

--- src/index.js
+++ src/index.js
@@ -9,26 +9,30 @@ const route = require('./routes');
 // img static file
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 
-app.use(express.urlencoded({
-  extended:true
-}));
+//
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 
 app.use(express.json());
 
-
 // HTTP checker
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // Template engine setup
-app.engine('hbs', engine({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
